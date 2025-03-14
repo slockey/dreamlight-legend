3e5d7bf39073d84c8b8add768b9af826202e443c
Steven Lockey <sclockey@gmail.com>
2025-03-14 11:23:50 -0400

    Initialized spring project

---
+++ src/main/java/com/slockey/dreamlightlegend/DreamlightLegendApplication.java
@@ -0,0 +1,13 @@
+package com.slockey.dreamlightlegend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamlightLegendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamlightLegendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slockey/dreamlightlegend/DreamlightLegendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slockey.dreamlightlegend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamlightLegendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
